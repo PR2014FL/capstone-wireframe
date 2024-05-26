43fa29d37df6d11c4d29a2155de09ee0a7691f10
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-05-26 13:11:04 -0400

    first draft of wireframe for capstone

---
+++ app.js
@@ -0,0 +1,15 @@
+function sendToBreath() {
+    window.location.href = "./breath.html"
+}
+
+function sendHome() {
+    window.location.href = "./index.html"
+}
+
+function sendToWellnessWheel() {
+    window.location.href = "./wellnessWheel.html"
+}
+
+function sendToGuidedMeditation() {
+    window.location.href = "./guidedMeditation.html"
+}
